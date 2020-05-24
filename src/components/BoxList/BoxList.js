a1a0c30bdf27a922dae195b5b013abf1a4edf120
Joey <jspahn83@gmail.com>
2020-05-24 12:20:23 -0600

    initialize state and array of color boxes

--- src/components/BoxList/BoxList.js
+++ src/components/BoxList/BoxList.js
@@ -3,25 +3,45 @@ import ColorBox from '../ColorBox/ColorBox';
 import './BoxList.css';
 
 class BoxList extends Component {
-    static defaultProps = {
-        bg: 'blue'
+    constructor(props) {
+        super(props);
+        this.state = {
+            numSquares: []
+        }  
     }
+
+    componentDidMount() {
+        // Generate array of Color Boxes for state.numSquares
+        this.setState({
+            numSquares: new Array(9).fill(
+                <ColorBox 
+                    bg={{backgroundColor: this.bgRand()}} 
+                    click={this.handleClick} 
+                />
+            )
+        })
+    }
+
+     // Generate random background color
+     bgRand = () => {
+        const randNum = () => {
+            return Math.floor(Math.random() * 256)
+        };
+        return `rgb(${randNum()}, ${randNum()}, ${randNum()})`;
+    }
+
+handleClick = () => {
+    console.log()
+}
     render() {
+        
         return(
             <div className='BoxList'>
                 <div className='BoxList-header'>
                     <h2>Guess the Color</h2>
                 </div>
                 <div className='BoxList-color-boxes'>
-                    <ColorBox />
-                    <ColorBox />
-                    <ColorBox />
-                    <ColorBox />
-                    <ColorBox />
-                    <ColorBox />
-                    <ColorBox />
-                    <ColorBox />
-                    <ColorBox />
+                    {this.state.numSquares}
                 </div>
             </div>
         );
--- src/components/ColorBox/ColorBox.js
+++ src/components/ColorBox/ColorBox.js
@@ -2,21 +2,9 @@ import React, { Component } from 'react';
 import './ColorBox.css';
 
 class ColorBox extends Component {
-    static defaultProps = {
-        bg: 'blue'
-    }
-
-    bgRand = () => {
-        const randNum = () => {
-            return Math.floor(Math.random() * 256)
-        };
-        return `rgb(${randNum()}, ${randNum()}, ${randNum()})`;
-    }
-
     render() {
         return(
-            <div onClick={this.changeBg} className={`ColorBox`} style={{backgroundColor: this.bgRand()}}>
-                {/* {this.bgRand()} */}
+            <div onClick={this.props.click} className={`ColorBox`} style={this.props.bg} >
             </div>
         );
     }
